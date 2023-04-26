0ae9a80ce3730c5c6714136a3757c4a3125347b1
K1NGAARON <gerardaaron1997@gmail.com>
2023-04-26 22:50:29 +0200

    changes

--- home/js/script.js
+++ home/js/script.js
@@ -8,21 +8,22 @@ const delay = 3000
 imageWrapper.style.setProperty('--per-view', perView)
 
 for(let i = 0; i < perView; i++) {
-    imageWrapper.insertAdjacentHTML('beforeend', imageItems[i].outerHTML)
+  imageWrapper.insertAdjacentHTML('beforeend', imageItems[i].outerHTML)
 }
 
 let autoScroll = setInterval(scrolling, delay)
 
 function scrolling() {
-    totalScroll++
-    if(totalScroll == imageLength + 1) {
-    clearInterval(autoScroll)
-    totalScroll = 1
-    imageWrapper.style.transition = '0s'
-    imageWrapper.style.left = '0'
-    autoScroll = setInterval(scrolling, delay)
-  }
-  const widthEl = document.querySelector('.image-wrapper > :first-child').offsetWidth + 24
-    imageWrapper.style.left = `-${totalScroll * widthEl}px`
-    imageWrapper.style.transition = '.3s'
+  totalScroll++
+  if(totalScroll == imageLength + 1) {
+  clearInterval(autoScroll)
+  totalScroll = 1
+  imageWrapper.style.transition = '0s'
+  imageWrapper.style.left = '0'
+  autoScroll = setInterval(scrolling, delay)
+}
+
+const widthEl = document.querySelector('.image-wrapper > :first-child').offsetWidth + 24
+  imageWrapper.style.left = `-${totalScroll * widthEl}px`
+  imageWrapper.style.transition = '.3s'
 }
